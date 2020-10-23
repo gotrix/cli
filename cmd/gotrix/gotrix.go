4dfb0f49801009ff009a095c0c1e052317e9a6d6
simon <even.simon@gmail.com>
2020-10-23 12:41:40 +0300

    work in progress

--- cmd/gotrix/gotrix.go
+++ cmd/gotrix/gotrix.go
@@ -4,49 +4,108 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
+	"math"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
+var (
+	appName string
+	fs      *flag.FlagSet
+
+	in       = &flags{}
+	commands = []*command{
+		{
+			name: "create-app",
+			exec: cmdCreateApp,
+			desc: "Create a new application.",
+		},
+		{
+			name: "create-component",
+			exec: cmdCreateComponent,
+			desc: "Create a new component.",
+		},
+		{
+			name: "build-components",
+			exec: cmdBuildComponents,
+			desc: `Build components so files. Path can be modified by -path option. The defaut is "./components".`,
+		},
+	}
+)
+
 func main() {
-	flag.BoolVar(&in.noColor, "no-color", false, "do not colorize output")
-	flag.BoolVar(&in.quiet, "quiet", false, "do not print any output")
-	flag.StringVar(&in.path, "path", "", "path to use")
-	flag.Parse()
-	var (
-		command = flag.Arg(0)
-		err     error
-	)
-	if command == "" {
-		flag.Usage()
+	appName = "gotrix"
+	fs = flag.NewFlagSet(appName, flag.ContinueOnError)
+	fs.Usage = usage
+	fs.BoolVar(&in.noColor, "no-color", false, "Do not colorize output (default false).")
+	fs.BoolVar(&in.quiet, "quiet", false, "Do not print any output (default false).")
+	fs.BoolVar(&in.help, "help", false, "Show this help.")
+	fs.StringVar(&in.path, "path", "", "Path to contents directory (default depends on the command).")
+	fs.Parse(os.Args[1:])
+	cmdName := fs.Arg(0)
+	if cmdName == "" || in.help == true {
+		fs.Usage()
 		return
 	}
-	switch command {
-	case "build-components":
-		err = cmdBuildComponents()
-	default:
-		err = fmt.Errorf(`unkown command "%s"`, command)
-	}
+	cmd, err := getCommand(cmdName)
 	if err != nil {
+		echoErr(err)
+	}
+	if err := cmd.exec(); err != nil {
 		echoErr(err)
 	}
 }
 
-func cmdBuildComponents() error {
-	var (
-		path = in.path
-		err  error
-	)
-	if path == "" {
-		path = "./components"
+func cmdCreateApp() error {
+	name := fs.Arg(1)
+	if name == "" {
+		fprintf(os.Stderr, "Usage:\n\n  %s %s [application name]\n",
+			Yellow.SPrint(appName),
+			Blue.SPrint("create-app"))
+		os.Exit(2)
 	}
-	path, err = filepath.Abs(path)
+	echo(Blue, `Creating new application "%s".`, name)
+	path, err := getPath(".")
 	if err != nil {
-		if os.IsNotExist(err) {
-			return fmt.Errorf(`%s: no such file or directory`, path)
-		}
+		return err
+	}
+	checkoutPath := filepath.Join(path, name)
+	repo := "github.com/gotrix/skull"
+	cmd := exec.Command("git", "clone",
+		"https://"+repo, checkoutPath)
+	outBuf := bytes.NewBuffer([]byte{})
+	cmd.Stderr = outBuf
+	cmd.Stdout = outBuf
+	echo(Blue, `Cloning %s to %s`, repo, checkoutPath)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf(`failed to clone %s: %v: %s`,
+			repo, err, strings.TrimRight(outBuf.String(), "\n"))
+	}
+	echo(Green, `Cloned %s to %s`, repo, checkoutPath)
+	outBuf.Reset()
+	cmd = exec.Command("git", "remote", "remove", "origin")
+	cmd.Stderr = outBuf
+	cmd.Stdout = outBuf
+	cmd.Dir = checkoutPath
+	echo(Blue, "Removing origins")
+	if err = cmd.Run(); err != nil {
+		return fmt.Errorf(`failed remove origins: %v: %s`,
+			err, strings.TrimRight(outBuf.String(), "\n"))
+	}
+	echo(Green, "Removed origins")
+	return nil
+}
+
+func cmdCreateComponent() error {
+	return nil
+}
+
+func cmdBuildComponents() error {
+	path, err := getPath("./components")
+	if err != nil {
 		return err
 	}
 	files, err := readPathsRecursive(path, ".go")
@@ -75,11 +134,45 @@ func cmdBuildComponents() error {
 		cmd.Stderr = outBuf
 		if err := cmd.Run(); err != nil {
 			return fmt.Errorf(`failed to build "%s": %v: %s`,
-				name, err, strings.TrimRight(outBuf.String(),"\n"))
+				name, err, strings.TrimRight(outBuf.String(), "\n"))
 		}
 		echo(Green, `successfully built "%s" to %s`, name, out)
 	}
 	return nil
+}
+
+func getPath(def string) (string, error) {
+	var (
+		path = in.path
+		err  error
+	)
+	if path == "" {
+		path = def
+	}
+	if strings.HasPrefix(path, "~") {
+		hd, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		path = hd + path[1:]
+	}
+	path, err = filepath.Abs(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", fmt.Errorf(`%s: no such file or directory`, path)
+		}
+		return "", err
+	}
+	return path, nil
+}
+
+func getCommand(name string) (*command, error) {
+	for _, cmd := range commands {
+		if cmd.name == name {
+			return cmd, nil
+		}
+	}
+	return nil, fmt.Errorf(`unknown command "%s"`, name)
 }
 
 func readPathsRecursive(dir string, suffix string) (list []string, err error) {
@@ -98,17 +191,19 @@ func readPathsRecursive(dir string, suffix string) (list []string, err error) {
 	return
 }
 
+func fprintf(w io.Writer, format string, args ...interface{}) {
+	_, _ = fmt.Fprintf(w, format, args...)
+}
+
 func echo(dot color, format string, args ...interface{}) {
 	if !in.quiet {
-		_, _ = fmt.Fprint(os.Stdout,
-			dot.SPrint("*"), " ", fmt.Sprintf(format, args...), "\n")
+		fprintf(os.Stdout, fmt.Sprintf("%s ", dot.SPrint("*"))+"%s\n", fmt.Sprintf(format, args...))
 	}
 }
 
 func echoErr(err error) {
 	if !in.quiet {
-		_, _ = fmt.Fprint(os.Stderr,
-			Red.SPrint("* error:"), " ", err.Error(), "\n")
+		fprintf(os.Stdout, fmt.Sprintf("%s ", Red.SPrint("* Error: "))+"%s\n", err.Error())
 	}
 	os.Exit(1)
 }
@@ -120,14 +215,59 @@ func multiSuffix(l int, s string) string {
 	return s + "s"
 }
 
-var (
-	in = &flags{}
-)
+func makeDesc(s string) string {
+	parts := strings.Split(strings.Trim(s, " "), " ")
+	res := make([]string, int(math.Ceil(float64(len(s)/60)))+1)
+	line := 0
+	for _, p := range parts {
+		if len(res[line]+" "+p) <= 60 {
+			if res[line] == "" {
+				res[line] += p
+			} else {
+				res[line] += " " + p
+			}
+		} else {
+			line++
+			res[line] += p
+		}
+	}
+	ret := make([]string, 0, len(res))
+	for _, v := range res {
+		if v != "" {
+			ret = append(ret, v)
+		}
+	}
+	return strings.Join(ret, "\n        ")
+}
+
+func usage() {
+	fprintf(os.Stdout, "Usage:\n")
+	fprintf(os.Stdout, "\n  %s [%s] %s\n", Yellow.SPrint(appName), Green.SPrint("options"), Blue.SPrint("command"))
+	fprintf(os.Stdout, "\nCommands:\n\n")
+	step := "        "
+
+	for _, cmd := range commands {
+		fprintf(os.Stdout, "  %s\n%s%s\n\n", Blue.SPrint(cmd.name), step, makeDesc(cmd.desc))
+	}
+	fprintf(os.Stdout, "Options:\n")
+	fs.VisitAll(func(f *flag.Flag) {
+		_, usage := flag.UnquoteUsage(f)
+		fprintf(os.Stdout, "\n  %s\n", Green.SPrint("-"+f.Name))
+		fprintf(os.Stdout, "        %s\n", makeDesc(usage))
+	})
+}
 
 type flags struct {
 	noColor bool
 	quiet   bool
+	help    bool
 	path    string
+}
+
+type command struct {
+	name string
+	desc string
+	exec func() error
 }
 
 type color string
@@ -144,10 +284,21 @@ func (c color) SPrint(s string) string {
 }
 
 const (
-	Black  color = "\u001b[30m"
-	Red    color = "\u001b[31m"
-	Green  color = "\u001b[32m"
+	// Black color.
+	Black color = "\u001b[30m"
+
+	// Red color.
+	Red color = "\u001b[31m"
+
+	// Green color.
+	Green color = "\u001b[32m"
+
+	// Yellow color.
 	Yellow color = "\u001b[33m"
-	Blue   color = "\u001b[34m"
-	Reset  color = "\u001b[0m"
+
+	// Blue color.
+	Blue color = "\u001b[34m"
+
+	// Reset color.
+	Reset color = "\u001b[0m"
 )
